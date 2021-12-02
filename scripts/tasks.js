09f0a97a2b6a22b815259812dccb848377b82e34
Juan <uo277963@uniovi.es>
2021-12-03 00:34:42 +0100

    tasks funcionando con localstorage

--- scripts/tasks.js
+++ scripts/tasks.js
@@ -1,36 +1,91 @@
-var tasks = null;
+var tasks = new Map();
 
-function display() {
-    this.readTasks();
-}
-
-function saveTask(text) {
-    tasks = JSON.parse(text);
-    showOnScreen()
-}
-
-function showOnScreen() {
-    tasks.tasks.forEach(element => {
-        $("#taskPanel").append("<div class=\"taskHoverable\">" + element.text +"</div>");
-    });
-    
+function loadTasks() {
+    readTasks();
+    showTasks();
 }
 
 function readTasks() {
-    readTextFile("resources/files/tasks.json", this.saveTask);
+    console.log("reading tasks...");
+    //readTextFile("resources/files/tasks.json", this.toMap, callback);
+    readFromLocalStorage();
 }
 
-function readTextFile(file, callback) {
-    var rawFile = new XMLHttpRequest();
-    rawFile.overrideMimeType("application/json");
-    rawFile.open("GET", file, true);
-    rawFile.onreadystatechange = function() {
-        if (rawFile.readyState === 4 && rawFile.status == "200") {
-            console.log("writing");
-            callback(rawFile.responseText);
-        }
+function readFromLocalStorage() {
+    console.log(localStorage);
+    loadMapFromLocal();
+}
+
+function loadMapFromLocal() {
+    var retrieved = localStorage.getItem("tasks");
+    tasks = new Map(JSON.parse(retrieved));
+    console.log(tasks);
+}
+
+function showTasks() {
+    console.log("showing tasks...");
+    tasks.forEach(
+        (value, key) => {{
+            var element = document.createElement("div");
+            element.setAttribute("id", key);
+            element.setAttribute("class", "taskHoverable");
+            element.setAttribute("onclick", "removeHTMLElement(this)");
+            element.innerText = value;
+            $("#taskPanel").append(element);
+        }}
+    );
+}
+
+function toMap(text) {
+    console.log("saving to map: " + text);
+    var arr = JSON.parse(text).tasks;
+    for (var i = 0; i < arr.length; i++) {
+        tasks.set(arr[i].id, arr[i].content);
     }
-    rawFile.send(null);
+    console.log(tasks);
 }
 
-window.addEventListener("onclick", display());
+function uuidv4() {
+    return ([1e7]+-1e3+-4e3+-8e3+-1e11).replace(/[018]/g, c =>
+      (c ^ crypto.getRandomValues(new Uint8Array(1))[0] & 15 >> c / 4).toString(16)
+    );
+  }
+
+function addTask() {
+    var key = uuidv4();
+    var value = $("#taskInput").val();
+    var element = document.createElement("div");
+    $("#taskInput").val("");
+
+    element.setAttribute("id", key);
+    element.setAttribute("class", "taskHoverable");
+    element.setAttribute("onclick", "removeHTMLElement(this)");
+    element.innerText = value;
+    $("#taskPanel").append(element);
+
+    tasks.set(key, value);
+
+    saveTasks();
+}
+
+function removeHTMLElement(element) {
+    tasks.delete(element.getAttribute("id"));
+    console.log("removed " + element.getAttribute("id"));
+    element.remove();
+    console.log(tasks);
+}
+
+function saveTasks() {
+    localStorage.setItem("tasks", JSON.stringify(Array.from(tasks.entries())));
+}
+
+window.addEventListener("onload", loadTasks());
+
+var input = document.getElementById("taskInput");
+input.addEventListener("keyup", function(event) {
+  if (event.keyCode === 13) {
+    event.preventDefault();
+    addTask();
+    //document.getElementById("enter").click();
+  }
+}); 
