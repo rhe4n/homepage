bf6840969543e400860d42cf43158b1ed4548697
rhe4n <rheandhont@gmail.com>
2022-03-20 11:39:16 +0100

    fuera intento de darkmode

--- scripts/main.js
+++ scripts/main.js
@@ -6,24 +6,24 @@ class HomePage {
     }
 
     loadDate() {
-        var options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric'};
+        var options = {
+            weekday: 'long',
+            year: 'numeric',
+            month: 'long',
+            day: 'numeric'
+        };
         var today = new Date();
         $(".date").text(today.toLocaleDateString("en-US", options));
-    }   
-    
+    }
+
     loadTime() {
         var now = new Date();
         $(".time").text(now.toLocaleTimeString("es-ES").slice(0, -3));
-    }    
+    }
 }
 
 function loadNav() {
     document.write("<nav><a href=\"index.html\">Home</a><a href=\"weather.html\">Weather</a><a href=\"applets.html\">Applets</a><a href=\"tasks.html\">Tasks</a></nav>");
-}
-
-function darkMode(checkbox) {
-    var element = document.body;
-    element.classList.toggle("darkMode");
 }
 
 var hp = new HomePage();
